e98bef40d7bfbd069ec4d9b9877bc9b4b88ca2b8
raam0001 <sivaramtamtam@gmail.com>
2019-08-14 10:55:55 +0530

    Update basic_health_check.sh

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -2,7 +2,7 @@
 
 for i in `seq 1 10`;
 do
-  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80`
+  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:8080`
   if [ "$HTTP_CODE" == "200" ]; then
     echo "Successfully pulled root page."
     exit 0;
